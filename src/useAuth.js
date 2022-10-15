a54a9ec1c3899e6a757ee58627110bf57abed848
Helen <le.helen19@gmail.com>
2022-10-14 20:41:32 -0700

    Edit useEffect hook to retrieve refresh token properly

--- src/useAuth.js
+++ src/useAuth.js
@@ -12,21 +12,45 @@ const useAuth = (code) => {
         code,
       })
       .then((res) => {
-        setAccessToken(res.data.access_token);
-        setRefreshToken(res.data.refresh_token);
-        setExpiresIn(res.data.expires_in);
+        setAccessToken(res.data.accessToken);
+        setRefreshToken(res.data.refreshToken);
+        setExpiresIn(res.data.expiresIn);
         // Remove 'code' from URL
         window.history.pushState({}, null, '/');
       })
       .catch((error) => {
         if (!error.response) {
-          console.log('network error');
+          console.log('network error from logging in');
         } else {
-          console.log('other error');
+          console.log('other error from logging in');
         }
         window.location = '/';
       });
   }, [code]);
+
+  useEffect(() => {
+    if (!refreshToken || !expiresIn) return;
+    // Refresh if token is close to expiration
+    axios
+      .post('http://localhost:3001/refresh', {
+        refreshToken,
+      })
+      .then((res) => {
+        // setAccessToken(res.data.access_token);
+        // setRefreshToken(res.data.refresh_token);
+        // setExpiresIn(res.data.expires_in);
+        // // Remove 'code' from URL
+        // window.history.pushState({}, null, '/');
+      })
+      .catch((error) => {
+        if (!error.response) {
+          console.log('network error from refresh');
+        } else {
+          console.log('other error from refresh');
+        }
+        window.location = '/';
+      });
+  }, [refreshToken, expiresIn]);
 
   return accessToken;
 };
