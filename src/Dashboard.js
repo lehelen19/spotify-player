02a70d6870bc35527570c8595770fdd1ba93dcc1
Helen <le.helen19@gmail.com>
2022-10-14 21:12:37 -0700

    Start dashboard skeleton with search bar and results

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import { useState } from 'react';
 import useAuth from './useAuth';
 
 const Dashboard = ({ code }) => {
   const accessToken = useAuth(code);
-  return <div>Hi from dashboard! {code}</div>;
+  const [search, setSearch] = useState('');
+
+  return (
+    <main>
+      <div className="search">
+        <label>
+          <input
+            type="search"
+            placeholder="Search songs/artists"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        </label>
+      </div>
+      <section className="results">
+        <h1>Results</h1>
+      </section>
+    </main>
+  );
 };
 
 export default Dashboard;
